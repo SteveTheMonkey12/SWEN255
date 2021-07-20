fef97d8b7dd63dc07e00142e43ab8e44e89007d8
Elliott Ramoo <ewramo@gmail.com>
2021-07-20 15:05:07 +1200

    remove old classes and add new

--- src/Board.java
+++ src/Board.java
@@ -4,8 +4,8 @@
 
 import java.util.*;
 
-// line 9 "model.ump"
-// line 66 "model.ump"
+// line 8 "model.ump"
+// line 45 "model.ump"
 public class Board
 {
 
@@ -15,8 +15,6 @@ public class Board
 
   //Board Associations
   private List<Game> games;
-  private List<Position> positions;
-  private List<Cell> cells;
 
   //------------------------
   // CONSTRUCTOR
@@ -25,8 +23,6 @@ public class Board
   public Board()
   {
     games = new ArrayList<Game>();
-    positions = new ArrayList<Position>();
-    cells = new ArrayList<Cell>();
   }
 
   //------------------------
@@ -60,66 +56,6 @@ public class Board
   public int indexOfGame(Game aGame)
   {
     int index = games.indexOf(aGame);
-    return index;
-  }
-  /* Code from template association_GetMany */
-  public Position getPosition(int index)
-  {
-    Position aPosition = positions.get(index);
-    return aPosition;
-  }
-
-  public List<Position> getPositions()
-  {
-    List<Position> newPositions = Collections.unmodifiableList(positions);
-    return newPositions;
-  }
-
-  public int numberOfPositions()
-  {
-    int number = positions.size();
-    return number;
-  }
-
-  public boolean hasPositions()
-  {
-    boolean has = positions.size() > 0;
-    return has;
-  }
-
-  public int indexOfPosition(Position aPosition)
-  {
-    int index = positions.indexOf(aPosition);
-    return index;
-  }
-  /* Code from template association_GetMany */
-  public Cell getCell(int index)
-  {
-    Cell aCell = cells.get(index);
-    return aCell;
-  }
-
-  public List<Cell> getCells()
-  {
-    List<Cell> newCells = Collections.unmodifiableList(cells);
-    return newCells;
-  }
-
-  public int numberOfCells()
-  {
-    int number = cells.size();
-    return number;
-  }
-
-  public boolean hasCells()
-  {
-    boolean has = cells.size() > 0;
-    return has;
-  }
-
-  public int indexOfCell(Cell aCell)
-  {
-    int index = cells.indexOf(aCell);
     return index;
   }
   /* Code from template association_MinimumNumberOfMethod */
@@ -204,170 +140,6 @@ public class Board
     }
     return wasAdded;
   }
-  /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfPositions()
-  {
-    return 0;
-  }
-  /* Code from template association_AddManyToManyMethod */
-  public boolean addPosition(Position aPosition)
-  {
-    boolean wasAdded = false;
-    if (positions.contains(aPosition)) { return false; }
-    positions.add(aPosition);
-    if (aPosition.indexOfBoard(this) != -1)
-    {
-      wasAdded = true;
-    }
-    else
-    {
-      wasAdded = aPosition.addBoard(this);
-      if (!wasAdded)
-      {
-        positions.remove(aPosition);
-      }
-    }
-    return wasAdded;
-  }
-  /* Code from template association_RemoveMany */
-  public boolean removePosition(Position aPosition)
-  {
-    boolean wasRemoved = false;
-    if (!positions.contains(aPosition))
-    {
-      return wasRemoved;
-    }
-
-    int oldIndex = positions.indexOf(aPosition);
-    positions.remove(oldIndex);
-    if (aPosition.indexOfBoard(this) == -1)
-    {
-      wasRemoved = true;
-    }
-    else
-    {
-      wasRemoved = aPosition.removeBoard(this);
-      if (!wasRemoved)
-      {
-        positions.add(oldIndex,aPosition);
-      }
-    }
-    return wasRemoved;
-  }
-  /* Code from template association_AddIndexControlFunctions */
-  public boolean addPositionAt(Position aPosition, int index)
-  {  
-    boolean wasAdded = false;
-    if(addPosition(aPosition))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfPositions()) { index = numberOfPositions() - 1; }
-      positions.remove(aPosition);
-      positions.add(index, aPosition);
-      wasAdded = true;
-    }
-    return wasAdded;
-  }
-
-  public boolean addOrMovePositionAt(Position aPosition, int index)
-  {
-    boolean wasAdded = false;
-    if(positions.contains(aPosition))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfPositions()) { index = numberOfPositions() - 1; }
-      positions.remove(aPosition);
-      positions.add(index, aPosition);
-      wasAdded = true;
-    } 
-    else 
-    {
-      wasAdded = addPositionAt(aPosition, index);
-    }
-    return wasAdded;
-  }
-  /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfCells()
-  {
-    return 0;
-  }
-  /* Code from template association_AddManyToManyMethod */
-  public boolean addCell(Cell aCell)
-  {
-    boolean wasAdded = false;
-    if (cells.contains(aCell)) { return false; }
-    cells.add(aCell);
-    if (aCell.indexOfBoard(this) != -1)
-    {
-      wasAdded = true;
-    }
-    else
-    {
-      wasAdded = aCell.addBoard(this);
-      if (!wasAdded)
-      {
-        cells.remove(aCell);
-      }
-    }
-    return wasAdded;
-  }
-  /* Code from template association_RemoveMany */
-  public boolean removeCell(Cell aCell)
-  {
-    boolean wasRemoved = false;
-    if (!cells.contains(aCell))
-    {
-      return wasRemoved;
-    }
-
-    int oldIndex = cells.indexOf(aCell);
-    cells.remove(oldIndex);
-    if (aCell.indexOfBoard(this) == -1)
-    {
-      wasRemoved = true;
-    }
-    else
-    {
-      wasRemoved = aCell.removeBoard(this);
-      if (!wasRemoved)
-      {
-        cells.add(oldIndex,aCell);
-      }
-    }
-    return wasRemoved;
-  }
-  /* Code from template association_AddIndexControlFunctions */
-  public boolean addCellAt(Cell aCell, int index)
-  {  
-    boolean wasAdded = false;
-    if(addCell(aCell))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfCells()) { index = numberOfCells() - 1; }
-      cells.remove(aCell);
-      cells.add(index, aCell);
-      wasAdded = true;
-    }
-    return wasAdded;
-  }
-
-  public boolean addOrMoveCellAt(Cell aCell, int index)
-  {
-    boolean wasAdded = false;
-    if(cells.contains(aCell))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfCells()) { index = numberOfCells() - 1; }
-      cells.remove(aCell);
-      cells.add(index, aCell);
-      wasAdded = true;
-    } 
-    else 
-    {
-      wasAdded = addCellAt(aCell, index);
-    }
-    return wasAdded;
-  }
 
   public void delete()
   {
@@ -376,18 +148,6 @@ public class Board
     for(Game aGame : copyOfGames)
     {
       aGame.removeBoard(this);
-    }
-    ArrayList<Position> copyOfPositions = new ArrayList<Position>(positions);
-    positions.clear();
-    for(Position aPosition : copyOfPositions)
-    {
-      aPosition.removeBoard(this);
-    }
-    ArrayList<Cell> copyOfCells = new ArrayList<Cell>(cells);
-    cells.clear();
-    for(Cell aCell : copyOfCells)
-    {
-      aCell.removeBoard(this);
     }
   }
 
--- src/Card.java
+++ src/Card.java
@@ -2,10 +2,9 @@
 /*This code was generated using the UMPLE 1.31.0.5692.1a9e80997 modeling language!*/
 
 
-import java.util.*;
 
-// line 21 "model.ump"
-// line 79 "model.ump"
+// line 19 "model.ump"
+// line 57 "model.ump"
 public class Card
 {
 
@@ -16,9 +15,6 @@ public class Card
   //Card Attributes
   private String name;
 
-  //Card Associations
-  private List<Player> players;
-
   //------------------------
   // CONSTRUCTOR
   //------------------------
@@ -26,7 +22,6 @@ public class Card
   public Card(String aName)
   {
     name = aName;
-    players = new ArrayList<Player>();
   }
 
   //------------------------
@@ -45,128 +40,9 @@ public class Card
   {
     return name;
   }
-  /* Code from template association_GetMany */
-  public Player getPlayer(int index)
-  {
-    Player aPlayer = players.get(index);
-    return aPlayer;
-  }
-
-  public List<Player> getPlayers()
-  {
-    List<Player> newPlayers = Collections.unmodifiableList(players);
-    return newPlayers;
-  }
-
-  public int numberOfPlayers()
-  {
-    int number = players.size();
-    return number;
-  }
-
-  public boolean hasPlayers()
-  {
-    boolean has = players.size() > 0;
-    return has;
-  }
-
-  public int indexOfPlayer(Player aPlayer)
-  {
-    int index = players.indexOf(aPlayer);
-    return index;
-  }
-  /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfPlayers()
-  {
-    return 0;
-  }
-  /* Code from template association_AddManyToManyMethod */
-  public boolean addPlayer(Player aPlayer)
-  {
-    boolean wasAdded = false;
-    if (players.contains(aPlayer)) { return false; }
-    players.add(aPlayer);
-    if (aPlayer.indexOfCard(this) != -1)
-    {
-      wasAdded = true;
-    }
-    else
-    {
-      wasAdded = aPlayer.addCard(this);
-      if (!wasAdded)
-      {
-        players.remove(aPlayer);
-      }
-    }
-    return wasAdded;
-  }
-  /* Code from template association_RemoveMany */
-  public boolean removePlayer(Player aPlayer)
-  {
-    boolean wasRemoved = false;
-    if (!players.contains(aPlayer))
-    {
-      return wasRemoved;
-    }
-
-    int oldIndex = players.indexOf(aPlayer);
-    players.remove(oldIndex);
-    if (aPlayer.indexOfCard(this) == -1)
-    {
-      wasRemoved = true;
-    }
-    else
-    {
-      wasRemoved = aPlayer.removeCard(this);
-      if (!wasRemoved)
-      {
-        players.add(oldIndex,aPlayer);
-      }
-    }
-    return wasRemoved;
-  }
-  /* Code from template association_AddIndexControlFunctions */
-  public boolean addPlayerAt(Player aPlayer, int index)
-  {  
-    boolean wasAdded = false;
-    if(addPlayer(aPlayer))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfPlayers()) { index = numberOfPlayers() - 1; }
-      players.remove(aPlayer);
-      players.add(index, aPlayer);
-      wasAdded = true;
-    }
-    return wasAdded;
-  }
-
-  public boolean addOrMovePlayerAt(Player aPlayer, int index)
-  {
-    boolean wasAdded = false;
-    if(players.contains(aPlayer))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfPlayers()) { index = numberOfPlayers() - 1; }
-      players.remove(aPlayer);
-      players.add(index, aPlayer);
-      wasAdded = true;
-    } 
-    else 
-    {
-      wasAdded = addPlayerAt(aPlayer, index);
-    }
-    return wasAdded;
-  }
 
   public void delete()
-  {
-    ArrayList<Player> copyOfPlayers = new ArrayList<Player>(players);
-    players.clear();
-    for(Player aPlayer : copyOfPlayers)
-    {
-      aPlayer.removeCard(this);
-    }
-  }
+  {}
 
 
   public String toString()
