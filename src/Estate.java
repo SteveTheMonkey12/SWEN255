f8d4ac9a416605746113629c7ade85d788569f30
Elliott Ramoo <ewramo@gmail.com>
2021-07-20 15:56:28 +1200

    add updated files

---
+++ src/Estate.java
@@ -0,0 +1,33 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.31.0.5692.1a9e80997 modeling language!*/
+
+
+
+// line 33 "model.ump"
+// line 75 "model.ump"
+public class Estate extends Card
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Estate(String aName)
+  {
+    super(aName);
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public void delete()
+  {
+    super.delete();
+  }
+
+}
---
+++ src/Weapon.java
@@ -0,0 +1,33 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.31.0.5692.1a9e80997 modeling language!*/
+
+
+
+// line 27 "model.ump"
+// line 69 "model.ump"
+public class Weapon extends Card
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Weapon(String aName)
+  {
+    super(aName);
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public void delete()
+  {
+    super.delete();
+  }
+
+}
