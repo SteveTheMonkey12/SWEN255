f3cdcdc0c122e8b1d5160a67b77c755c9002749c
Elliott Ramoo <ewramo@gmail.com>
2021-07-25 15:25:08 +1200

    remove position

--- src/Game.java
+++ src/Game.java
@@ -13,12 +13,8 @@ public class Game
   // MEMBER VARIABLES
   //------------------------
 
-  //Game Attributes
-  private HashMap position;
-
   //Game Associations
   private List<Player> players;
-  private List<Position> positions;
   private List<Board> boards;
   private List<Weapon> weapons;
   private List<Estate> estates;
@@ -39,11 +35,9 @@ public class Game
   // CONSTRUCTOR
   //------------------------
 
-  public Game(HashMap aPosition)
+  public Game()
   {
-    position = aPosition;
     players = new ArrayList<Player>();
-    positions = new ArrayList<Position>();
     boards = new ArrayList<Board>();
     cards = new ArrayList<Card>();
     weapons = new ArrayList<Weapon>();
@@ -56,19 +50,6 @@ public class Game
   // INTERFACE
   //------------------------
 
-  public boolean setPosition(HashMap aPosition)
-  {
-    boolean wasSet = false;
-    position = aPosition;
-    wasSet = true;
-    return wasSet;
-  }
-
-  public HashMap getPosition()
-  {
-    return position;
-  }
-  
   /* Code from template association_GetMany */
   public Weapon getWeapon(int index)
   {
@@ -161,36 +142,7 @@ public class Game
     int index = players.indexOf(aPlayer);
     return index;
   }
-  /* Code from template association_GetMany */
-  public Position getPosition(int index)
-  {
-    Position aPosition = positions.get(index);
-    return aPosition;
-  }
-
-  public List<Position> getPositions()
-  {
-    List<Position> newPositions = Collections.unmodifiableList(positions);
-    return newPositions;
-  }
-
-  public int numberOfPositions()
-  {
-    int number = positions.size();
-    return number;
-  }
-
-  public boolean hasPositions()
-  {
-    boolean has = positions.size() > 0;
-    return has;
-  }
-
-  public int indexOfPosition(Position aPosition)
-  {
-    int index = positions.indexOf(aPosition);
-    return index;
-  }
+  
   /* Code from template association_GetMany */
   public Board getBoard(int index)
   {
@@ -481,88 +433,7 @@ public class Game
     }
     return wasAdded;
   }
-  /* Code from template association_MinimumNumberOfMethod */
-  public static int minimumNumberOfPositions()
-  {
-    return 0;
-  }
-  /* Code from template association_AddManyToManyMethod */
-  public boolean addPosition(Position aPosition)
-  {
-    boolean wasAdded = false;
-    if (positions.contains(aPosition)) { return false; }
-    positions.add(aPosition);
-    if (aPosition.indexOfGame(this) != -1)
-    {
-      wasAdded = true;
-    }
-    else
-    {
-      wasAdded = aPosition.addGame(this);
-      if (!wasAdded)
-      {
-        positions.remove(aPosition);
-      }
-    }
-    return wasAdded;
-  }
-  /* Code from template association_RemoveMany */
-  public boolean removePosition(Position aPosition)
-  {
-    boolean wasRemoved = false;
-    if (!positions.contains(aPosition))
-    {
-      return wasRemoved;
-    }
-
-    int oldIndex = positions.indexOf(aPosition);
-    positions.remove(oldIndex);
-    if (aPosition.indexOfGame(this) == -1)
-    {
-      wasRemoved = true;
-    }
-    else
-    {
-      wasRemoved = aPosition.removeGame(this);
-      if (!wasRemoved)
-      {
-        positions.add(oldIndex,aPosition);
-      }
-    }
-    return wasRemoved;
-  }
-  /* Code from template association_AddIndexControlFunctions */
-  public boolean addPositionAt(Position aPosition, int index)
-  {  
-    boolean wasAdded = false;
-    if(addPosition(aPosition))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfPositions()) { index = numberOfPositions() - 1; }
-      positions.remove(aPosition);
-      positions.add(index, aPosition);
-      wasAdded = true;
-    }
-    return wasAdded;
-  }
-
-  public boolean addOrMovePositionAt(Position aPosition, int index)
-  {
-    boolean wasAdded = false;
-    if(positions.contains(aPosition))
-    {
-      if(index < 0 ) { index = 0; }
-      if(index > numberOfPositions()) { index = numberOfPositions() - 1; }
-      positions.remove(aPosition);
-      positions.add(index, aPosition);
-      wasAdded = true;
-    } 
-    else 
-    {
-      wasAdded = addPositionAt(aPosition, index);
-    }
-    return wasAdded;
-  }
+  
   /* Code from template association_MinimumNumberOfMethod */
   public static int minimumNumberOfBoards()
   {
@@ -768,12 +639,6 @@ public class Game
     estates.clear();
     ArrayList<Player> copyOfPlayers = new ArrayList<Player>(players);
     players.clear();
-    ArrayList<Position> copyOfPositions = new ArrayList<Position>(positions);
-    positions.clear();
-    for(Position aPosition : copyOfPositions)
-    {
-      aPosition.removeGame(this);
-    }
     ArrayList<Board> copyOfBoards = new ArrayList<Board>(boards);
     boards.clear();
     for(Board aBoard : copyOfBoards)
@@ -781,13 +646,6 @@ public class Game
       aBoard.removeGame(this);
     }
     cards.clear();
-  }
-
-
-  public String toString()
-  {
-    return super.toString() + "["+ "]" + System.getProperties().getProperty("line.separator") +
-            "  " + "position" + "=" + (getPosition() != null ? !getPosition().equals(this)  ? getPosition().toString().replaceAll("  ","    ") : "this" : "null");
   }
   
   //Elliott's guess stuff
