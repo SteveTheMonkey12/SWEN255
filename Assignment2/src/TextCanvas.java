ce36e34297beb2b21b38dfaf9c9853823215d0a3
rubyratana <87345280+rubyratana@users.noreply.github.com>
2021-08-11 16:51:51 +1200

    Update TextCanvas.java

--- Assignment2/src/TextCanvas.java
+++ Assignment2/src/TextCanvas.java
@@ -4,15 +4,16 @@ import java.util.List;
 
 /**
  * This canvas will show the cards for different player.
+ * 
  * @author pengailin
  *
  */
 public class TextCanvas extends Canvas {
-	
+
 	private String player;
 	private int steps;
 	private List<Item> cards;
-	
+
 	/**
 	 * Construct a canvas to visually display a given board.
 	 *
@@ -21,30 +22,33 @@ public class TextCanvas extends Canvas {
 	public TextCanvas() {
 		setBounds(0, 960, 400, 960);
 	}
-	
+
 	public void setPlayer(Player player) {
 		this.player = player.getName();
 	}
-	
+
 	public void setSteps(int steps) {
 		this.steps = steps;
 	}
-	
+
 	public void setCards(List<Item> cards) {
 		this.cards = cards;
 	}
-	
+
 	public void paint(Graphics g) {
-		//show the name of the player
-		g.drawString(player+"'s turn", 10, 20);
-		//show the steps remaining
-		g.drawString("You have "+steps+" left.", 10, 40);
-		//show the cards on player's hand
-		g.drawString("Cards in hand: ", 10 , 60);
+		// show the name of the player
+		g.drawString(player + "'s turn", 10, 20);
+		// show the steps remaining
+		g.drawString("You have " + steps + " left.", 10, 40);
+		// show the cards on player's hand
+		g.drawString("Cards in hand: ", 10, 60);
 		int gap = 20;
-		for(int i =0; i<cards.size(); i++) {
-			g.drawString(cards.get(i).getName(), gap+10, 60);
-		}	
+		if (cards != null) {
+			for (int i = 0; i < cards.size(); i++) {
+				g.drawString(cards.get(i).getName(), 10, 60 + gap);
+				gap+=20;
+			}
+		}
 	}
 
 }
