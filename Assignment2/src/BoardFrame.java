5352b56292665ff8a137990c2aac9d7aad1a87c5
rubyratana <87345280+rubyratana@users.noreply.github.com>
2021-08-12 16:18:26 +1200

    Update BoardFrame.java

--- Assignment2/src/BoardFrame.java
+++ Assignment2/src/BoardFrame.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
